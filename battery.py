2b5d6b6c8366b18e55ddb09b2e013457706f1bbf
starsenea <141626761+starsenea@users.noreply.github.com>
2023-08-08 11:09:28 +0400

    battery.py

--- battery.py
+++ battery.py
@@ -1,10 +1,7 @@
-from abc import ABC, abstractmethod
+from abc import ABC
 
-class Battery(Car):
-  def __init__(self, last_service_date, current_date):
-      self.last_service_date = last_service_date
-      self.current_date = current_date
-  def battery_needs_service(self):
-        pass  
-  
+
+class Battery(ABC):
+    def needs_service(self):
+        pass
 
