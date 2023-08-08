07e75224e82350d49f869dc5c72604a0e77602f9
starsenea <141626761+starsenea@users.noreply.github.com>
2023-08-08 12:32:10 +0400

    Update carrigan.py

--- tires/carrigan.py
+++ tires/carrigan.py
@@ -2,8 +2,10 @@ from tire import Tire
 
 class CarriganTire(Tire):
     def __init__(self, tire_sensor_data):
-        super().__init__(tire_sensor_data)
         self.tire_sensor_data = tire_sensor_data
 
-    def tire_needs_service(self):
-        return any(map(lambda current_tire: current_tire >= 0.9, self.tire_sensor_data))
+    def needs_service(self):
+        for tire in self.tire_sensor_data:
+            if tire >= 0.9:
+                return True
+        return False
